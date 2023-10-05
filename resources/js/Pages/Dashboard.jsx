4ef454972aefa724ba0b018117b5b7314857617f
DumCare <waimar.tincho@we-interactive.com>
2023-10-05 16:04:07 +0700

    Back original

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -15,12 +15,6 @@ export default function Dashboard({ auth, apitoken }) {
         >
             <Head title="Dashboard" />
 
-            <div>Merge Test</div>
-
-            <div>Test</div>
-            <div>Test</div>
-            <div>Test</div>
-
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-4">
                     <div className=" overflow-hidden sm:rounded-lg">
