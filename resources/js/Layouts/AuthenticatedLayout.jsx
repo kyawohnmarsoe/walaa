4b6959138e1749dc1c172a1a5269de436b1ef1ff
Kyaw Ohn Mar Soe <kyawohnmarsoe@outlook.com>
2023-10-25 14:12:30 +0630

    update menu

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -107,43 +107,6 @@ export default function Authenticated ({ user, header, children })
 
 
 
-                                <div className="sm:flex sm:items-center">
-                                    <div className="relative">
-                                        <Dropdown>
-                                            <Dropdown.Trigger>
-                                                <span className="inline-flex rounded-md">
-                                                    <button
-                                                        type="button"
-                                                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                                    >
-                                                        Reports
-
-                                                        <svg
-                                                            className="ml-2 -mr-0.5 h-4 w-4"
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            viewBox="0 0 20 20"
-                                                            fill="currentColor"
-                                                        >
-                                                            <path
-                                                                fillRule="evenodd"
-                                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                                clipRule="evenodd"
-                                                            />
-                                                        </svg>
-                                                    </button>
-
-                                                </span>
-                                            </Dropdown.Trigger>
-
-                                            <Dropdown.Content align={ 'left' }>
-                                                <Dropdown.Link href={ route('usersessions') }>User Sessions</Dropdown.Link>
-                                                <Dropdown.Link href={ route('prepaid.needed') }>Prepaid Needed</Dropdown.Link >
-                                                {/* <Dropdown.Link href={ route('deposit.statement') }>Account Statement</Dropdown.Link > */ }
-                                                <Dropdown.Link href={ route('deposit.transfer') }>Balance Transfer</Dropdown.Link >
-                                            </Dropdown.Content>
-                                        </Dropdown>
-                                    </div>
-                                </div>
 
                                 <div className="sm:flex sm:items-center">
                                     <div className="relative">
@@ -181,41 +144,7 @@ export default function Authenticated ({ user, header, children })
                                     </div>
                                 </div>
 
-                                <div className="sm:flex sm:items-center">
-                                    <div className="relative">
-                                        <Dropdown>
-                                            <Dropdown.Trigger>
-                                                <span className="inline-flex rounded-md">
-                                                    <button
-                                                        type="button"
-                                                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                                    >
-                                                        Report
 
-                                                        <svg
-                                                            className="ml-2 -mr-0.5 h-4 w-4"
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            viewBox="0 0 20 20"
-                                                            fill="currentColor"
-                                                        >
-                                                            <path
-                                                                fillRule="evenodd"
-                                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                                clipRule="evenodd"
-                                                            />
-                                                        </svg>
-                                                    </button>
-
-                                                </span>
-                                            </Dropdown.Trigger>
-
-                                            <Dropdown.Content align={ 'left' }>
-                                                <Dropdown.Link href={ route('usersessions') }>User Sessions</Dropdown.Link>
-                                                <Dropdown.Link href={ route('prepaid.needed') }>Prepaid Needed</Dropdown.Link >
-                                            </Dropdown.Content>
-                                        </Dropdown>
-                                    </div>
-                                </div>
 
                                 {/* <NavLink href={ route('users.online') } active={ route().current('users.online') }>
                                     Online Users
